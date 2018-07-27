4621d454cf42d07a0501e417f2c6ba01adc038c2
Nikita Shchypylov <shchypylov@gmail.com>
2018-07-27 22:46:36 +0300

    final alpha

--- echobot.py
+++ echobot.py
@@ -1,7 +1,9 @@
 import json
 import requests
+import time
+import urllib
 
-TOKEN = "<your API key>"
+TOKEN = "<yours key>"
 URL = "https://api.telegram.org/bot{}/".format(TOKEN)
 
 
@@ -17,8 +19,27 @@ def get_json_from_url(url):
     return js
 
 
-def get_updates():
-    url = URL + "getUpdates"
+def get_last_update_id(updates):
+    update_ids = []
+    for update in updates["result"]:
+        update_ids.append(int(update["update_id"]))
+    return max(update_ids)
+
+
+def echo_all(updates):
+    for update in updates["result"]:
+        try:
+            text = update["message"]["text"]
+            chat = update["message"]["chat"]["id"]
+            send_message(text, chat)
+        except Exception as e:
+            print(e)
+
+
+def get_updates(offset=None):
+    url = URL + "getUpdates?timeout=100"
+    if offset:
+        url += "&offset={}".format(offset)
     js = get_json_from_url(url)
     return js
 
@@ -26,15 +47,28 @@ def get_updates():
 def get_last_chat_id_and_text(updates):
     num_updates = len(updates["result"])
     last_update = num_updates - 1
-    message = updates["result"][last_update]["message"]["text"]
+    text = updates["result"][last_update]["message"]["text"]
     chat_id = updates["result"][last_update]["message"]["chat"]["id"]
-    return message, chat_id
+    return text, chat_id
 
 
-def send_message(message, chat_id):
-    url = URL + "sendMessage?text={}&chat_id={}".format(message, chat_id)
+def send_message(text, chat_id):
+    text = urllib.parse.quote_plus(text)
+    url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
     get_url(url)
 
 
-text, chat = get_last_chat_id_and_text(get_updates())
-send_message(text, chat)
+def main():
+    last_update_id = None
+    while True:
+        print("getting updates")
+        updates = get_updates(last_update_id)
+        if (len(updates["result"])) > 0:
+            last_update_id = get_last_update_id(updates) + 1
+            print(last_update_id)
+        echo_all(updates)
+        time.sleep(0.5)
+
+
+if __name__ == "__main__":
+    main()
